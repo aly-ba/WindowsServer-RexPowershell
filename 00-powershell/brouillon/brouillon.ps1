c7642747817535508a8fc0c07763e3ffe01edd08
Network & Sys Admin - Full Stack .Net Developer <7081719+aly-ba@users.noreply.github.com>
2018-11-26 16:57:12 +0100

    working brouillon essentially process

--- 00-powershell/brouillon/brouillon.ps1
+++ 00-powershell/brouillon/brouillon.ps1
@@ -1,6 +1,16 @@
-﻿Get-Process | 
+﻿
+#prcoess
+Get-Process | 
 foreach{
         $proc = $_;   $proc.Modules |
         where { $_.ModuleName -eq "mscoree.dll" } |
         foreach { echo $proc }
 }
+# 
+echo "Lowering installer process priorities"
+Get-Process msiexec -ErrorAction Stop | 
+    foreach { $_.PriorityClass = "BelowNormal" }
+Read-Host -prompt "Press Enter to restore installer priorities"
+Get-Process msiexec -ErrorAction SilentlyContinue | 
+    foreach { $_.PriorityClass = "Normal" }
+
