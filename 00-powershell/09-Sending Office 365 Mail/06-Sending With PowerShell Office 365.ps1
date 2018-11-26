e2b1d1f8caf30b31e2bb34b7abcbd3dd853761d1
Network & Sys Admin - Full Stack .Net Developer <7081719+aly-ba@users.noreply.github.com>
2018-11-26 16:23:17 +0100

    help  and others things

---
+++ 00-powershell/09-Sending Office 365 Mail/06-Sending With PowerShell Office 365.ps1
@@ -0,0 +1,12 @@
+﻿$SMTPServer = "outlook.office365.com"
+$SMTPExpediteur = "sender@lds.fr"
+$SMTPDestinataire = "my-contact@domain.fr"
+$Credentials = Get-Credential
+$EmailSubject = "E-mail via Office 365"
+$EmailContent = "Bonjour, voici un e-mail envoyé via authentification sur Office 365"
+
+
+Send-MailMessage -Verbose -SmtpServer $SMTPServer `
+   -From $SMTPExpediteur -To $SMTPDestinataire -Credential $Credentials `
+   -Subject $EmailSubject `
+   -Body $EmailContent -BodyAsHtml -UseSsl -Port 587
---
+++ 00-powershell/brouillon/brouillon.ps1
@@ -0,0 +1,6 @@
+﻿Get-Process | 
+foreach{
+        $proc = $_;   $proc.Modules |
+        where { $_.ModuleName -eq "mscoree.dll" } |
+        foreach { echo $proc }
+}
