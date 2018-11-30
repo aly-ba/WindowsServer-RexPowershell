b955626b2a67e109460119fe08c806d6d34e016c
Network & Sys Admin - Full Stack .Net Developer <7081719+aly-ba@users.noreply.github.com>
2018-11-30 09:36:26 +0100

    new brouillon

---
+++ 00-powershell/brouillon/Ftp.ps1
@@ -0,0 +1,17 @@
+﻿# .\Put-Ftp.ps1 -url ftp://deshev.com/Get-Ftp.ps1
+
+# -file (gi Get-Ftp.ps1).FullName
+
+# -user ftptest -password testpass
+
+
+
+param ($url, $file, $username, $passwor) 
+
+$client = New-Object System.Net.WebClient
+
+$credentials= New-object  System.Net.NetworkCredential `
+                -arg $userName , $password
+$credentials.Credentials = $credentials
+
+$client.UplaidFile($url, $file)
